cfcca94a51b0c3f7b7adc291cb7dbcd5ccea2ce7
Viktor <viktor.leher@gmail.com>
2019-03-17 20:29:35 +0100

    very basic layout

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,15 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './modules/app-routing.module';
 import { AppComponent } from './app.component';
 import {MaterialModule} from "./modules/material.module";
+import { SearchbarComponent } from './components/searchbar/searchbar.component';
+import { ImageDetailsComponent } from './components/image-details/image-details.component';
+import { ImageSliderComponent } from './components/image-slider/image-slider.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchbarComponent,
+    ImageDetailsComponent,
+    ImageSliderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MaterialModule
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/modules/material.module.ts
+++ frontend/src/app/modules/material.module.ts
@@ -1,31 +1,54 @@
 import {ModuleWithProviders, NgModule, Optional, SkipSelf} from '@angular/core';
 import {
-  MatButtonModule,
-  MatCardModule,
-  MatDatepickerModule,
-  MatInputModule,
-  MatNativeDateModule,
-} from "@angular/material";
-import {NoopAnimationsModule} from "@angular/platform-browser/animations";
+    MatButtonModule,
+    MatCardModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatNativeDateModule,
+    MatOptionModule,
+    MatAutocompleteModule,
+    MatTabsModule
+} from '@angular/material';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 
 
 @NgModule({
-  declarations: [],
-  imports: [MatButtonModule, MatCardModule, NoopAnimationsModule, MatInputModule, MatDatepickerModule, MatNativeDateModule],
-  exports: [MatButtonModule, MatCardModule, NoopAnimationsModule, MatInputModule, MatDatepickerModule, MatNativeDateModule]
+    declarations: [],
+    imports: [
+        MatButtonModule,
+        MatCardModule,
+        NoopAnimationsModule,
+        MatInputModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatOptionModule,
+        MatAutocompleteModule,
+        MatTabsModule
+    ],
+    exports: [
+        MatButtonModule,
+        MatCardModule,
+        NoopAnimationsModule,
+        MatInputModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatOptionModule,
+        MatAutocompleteModule,
+        MatTabsModule
+    ]
 })
 export class MaterialModule {
 
-  public static forRoot(): ModuleWithProviders {
-    return {
-      ngModule: MaterialModule,
-      providers: []
+    public static forRoot(): ModuleWithProviders {
+        return {
+            ngModule: MaterialModule,
+            providers: []
+        };
     }
-  }
 
-  constructor(@Optional() @SkipSelf() parentModule: MaterialModule) {
-    if (parentModule) {
-      throw new Error('MaterialModule is already loaded');
+    constructor(@Optional() @SkipSelf() parentModule: MaterialModule) {
+        if (parentModule) {
+            throw new Error('MaterialModule is already loaded');
+        }
     }
-  }
 }
